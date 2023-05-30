d371fcec2204a47072a42a30b89ce39bd5d316a6
Muhammad Bin Mokhtar <120654723+MuhdMokhtar@users.noreply.github.com>
2023-05-30 23:56:45 +0800

    update verifyuser.js

--- Template for module/js/verifyuser.js
+++ Template for module/js/verifyuser.js
@@ -5,12 +5,10 @@
   // Add a click event listener to the button
   verifyButton.addEventListener('click', function() {
     // Perform eligibility verification logic here
-    const isAsalPahang = true; // Replace with your logic to check if both partners are from Pahang
-    const maleIncome = 5500.00; // Replace with the actual income value for the male partner
-
-    // Additional conditions
-    const isPoligami = false; // Replace with your logic to check if the person is involved in polygamy
-    const isNikahTanpaKebenaran = false; // Replace with your logic to check if the person has married without permission
+    const isAsalPahang = true; 
+    const maleIncome = 5500.00; 
+    const isPoligami = false; 
+    const isNikahTanpaKebenaran = false; 
 
     // Display a pop-up message based on eligibility
     if (isAsalPahang && maleIncome <= 5000.00 && !isPoligami && !isNikahTanpaKebenaran) {
